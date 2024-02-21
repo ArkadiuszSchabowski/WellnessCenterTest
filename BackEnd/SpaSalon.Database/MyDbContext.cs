b354260cd1af0acae1970d4194d6b9899ae9e479
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-02-21 11:32:58 +0100

    Add entities to database

--- BackEnd/SpaSalon.Database/MyDbContext.cs
+++ BackEnd/SpaSalon.Database/MyDbContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SpaSalon.Database.Entities;
 
 namespace SpaSalon.Database
 {
     public class MyDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
         public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
         {
 
