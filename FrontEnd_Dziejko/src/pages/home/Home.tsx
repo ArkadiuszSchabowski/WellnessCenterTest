7a4e1c89967d49d97e0d7ed5ce46f028828323f4
Your Name~Dziejko <you@example.com>
2024-02-24 23:23:26 +0000

    added fetch button

--- FrontEnd_Dziejko/src/pages/home/Home.tsx
+++ FrontEnd_Dziejko/src/pages/home/Home.tsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import Hero from "./Hero";
+import { Button } from "react-bootstrap";
 
 function Home() {
+    const [data, setData] = useState(null);
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://localhost:7004/api/massage');
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      }
+    };
     return (
         <>
             <Hero />
-            <div style={{ minHeight: "200vh" }}>Home</div>
+            <div style={{ minHeight: "50vh" }}>Home</div>
+            {data&& JSON.stringify(data)}
+            <Button onClick={fetchData}>Kliknij se</Button>
         </>
     );
 }
