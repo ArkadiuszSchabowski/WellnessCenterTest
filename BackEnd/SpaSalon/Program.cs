a8aa7bb4ffba4a39c72515ae5acf492fe9feaeda
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-02-25 02:17:15 +0100

    Registration IUserService in DI contener

--- BackEnd/SpaSalon/Program.cs
+++ BackEnd/SpaSalon/Program.cs
@@ -21,6 +21,7 @@ namespace SpaSalon
             builder.Services.AddDbContext<MyDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SpaSalonConnectionString")));
             builder.Services.AddScoped<IMassageService, MassageService>();
             builder.Services.AddScoped<IAccountService, AccountService>();
+            builder.Services.AddScoped<IUserService, UserService>();
             builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
             builder.Services.AddSwaggerGen();
             builder.Services.AddScoped<ErrorHandlingMiddleware>();
