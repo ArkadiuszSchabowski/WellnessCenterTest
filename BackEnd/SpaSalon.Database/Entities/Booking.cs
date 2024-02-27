e334b2b30b2ca00d1bca33a991272c7b0d089dce
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-02-27 15:35:58 +0100

    add entity Booking

---
+++ BackEnd/SpaSalon.Database/Entities/Booking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaSalon.Database.Entities
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public string Price { get; set; }
+        public DateTime Date { get; set; }
+        public bool isPaid { get; set; }
+    }
+}
